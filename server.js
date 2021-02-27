4c6d7ca9054e11f3a154391d79b04643b968c79e
saslam23 <saadaslam13@gmail.com>
2021-02-26 19:59:05 -0600

    final changes

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({path: '.env'});
+require('dotenv').config();
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
